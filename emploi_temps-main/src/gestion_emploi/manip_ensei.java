b63c7a333987d479674b8e6680107e431364ddc1
Hend-Haddad <139687097+Hend-Haddad@users.noreply.github.com>
2025-04-16 21:57:53 +0100

    Update manip_ensei.java

--- emploi_temps-main/src/gestion_emploi/manip_ensei.java
+++ emploi_temps-main/src/gestion_emploi/manip_ensei.java
@@ -9,16 +9,7 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import net.proteanit.sql.DbUtils;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Achref
- */
 public class manip_ensei extends javax.swing.JFrame {
 
     /**
