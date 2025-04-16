190a0a729dd64195868edad322744783b76b472f
Hend-Haddad <139687097+Hend-Haddad@users.noreply.github.com>
2025-04-16 21:57:00 +0100

    Update index.java

--- emploi_temps-main/src/gestion_emploi/index.java
+++ emploi_temps-main/src/gestion_emploi/index.java
@@ -9,16 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Achref
- */
 public class index extends javax.swing.JFrame {
 
     /**
